ed3f6eac270501d2934eead105bf7c14d56d90f3
baju-che <juliusgacheru021@gmail.com>
2026-06-26 16:15:56 +0300

    Add GitHub Actions CI pipeline

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,10 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
